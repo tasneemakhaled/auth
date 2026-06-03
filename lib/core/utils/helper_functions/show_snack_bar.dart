0d1ade2d35c48bee4a37e5201f8bc726ba3554ca
tasneemakhaled <khaledtasneem402@gmail.com>
2026-06-04 01:13:11 +0300

    create custom fields done

--- lib/core/utils/helper_functions/show_snack_bar.dart
+++ lib/core/utils/helper_functions/show_snack_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ShowBar extends StatelessWidget {
-  const ShowBar({super.key, required this.text});
+class ShowSnackBar extends StatelessWidget {
+  const ShowSnackBar({super.key, required this.text});
   final String text;
   @override
   Widget build(BuildContext context) {
---
+++ lib/features/auth/presentation/views/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:auth/core/utils/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text, this.onPressed});
+  final String text;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(text, style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
