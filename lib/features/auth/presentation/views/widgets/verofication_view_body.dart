33b5367c43e15b90a2876a6f3a1acc88976c6551
tasneemakhaled <khaledtasneem402@gmail.com>
2026-06-04 01:15:23 +0300

    create body of all views

---
+++ lib/features/auth/presentation/views/widgets/verofication_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VeroficationViewBody extends StatelessWidget {
+  const VeroficationViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
