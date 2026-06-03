0244f34f4c4b305602e81d3bf7aff8793a367012
tasneemakhaled <khaledtasneem402@gmail.com>
2026-06-03 20:46:04 +0300

    add main views of project

---
+++ lib/features/auth/presentation/views/reset_password_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResetPasswordView extends StatelessWidget {
+  const ResetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
