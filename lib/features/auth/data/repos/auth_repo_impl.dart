fcc0e4b91844d3ba85b739a46c4c20ebbdcb3b78
tasneemakhaled <khaledtasneem402@gmail.com>
2026-06-06 22:31:40 +0300

    auth repo impl done

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,47 +1,73 @@
+import 'package:auth/core/utils/services/auth_service.dart';
 import 'package:auth/core/utils/services/failure.dart';
 import 'package:auth/features/auth/domain/entites/user_entity.dart';
 import 'package:auth/features/auth/domain/repos/auth_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  final AuthService authService;
+
+  AuthRepoImpl({required this.authService});
+  @override
+  Future<Either<Failure, UserEntity>> signUpUser({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var res = await authService.signUp(email: email, password: password);
+      return right(UserEntity(email: email, password: password));
+    } on Exception catch (e) {
+      return left(Failure(errorMessage: e.toString()));
+    }
+  }
+
   @override
   Future<Either<Failure, UserEntity>> logInUser({
     required String email,
     required String password,
-  }) {
-    // TODO: implement logInUser
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, void>> resetPassword({required String newPassword}) {
-    // TODO: implement resetPassword
-    throw UnimplementedError();
+  }) async {
+    try {
+      var res = await authService.logIn(email: email, password: password);
+      return right(UserEntity(email: email, password: password));
+    } on Exception catch (e) {
+      return left(Failure(errorMessage: e.toString()));
+    }
   }
 
   @override
   Future<Either<Failure, void>> sendPasswordResetEmail({
     required String email,
-  }) {
-    // TODO: implement sendPasswordResetEmail
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, UserEntity>> signUpUser({
-    required String email,
-    required String password,
-  }) {
-    // TODO: implement signUpUser
-    throw UnimplementedError();
+  }) async {
+    try {
+      var res = await authService.sendPasswordResetEmail(email);
+      return right(null);
+    } on Exception catch (e) {
+      return left(Failure(errorMessage: e.toString()));
+    }
   }
 
   @override
   Future<Either<Failure, void>> verifyOTP({
     required String email,
     required String otp,
-  }) {
-    // TODO: implement verifyOTP
-    throw UnimplementedError();
+  }) async {
+    try {
+      var res = await authService.verifyOTP(email: email, otp: otp);
+      return right(null);
+    } on Exception catch (e) {
+      return left(Failure(errorMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> resetPassword({
+    required String newPassword,
+  }) async {
+    try {
+      await authService.resetPassword(password: newPassword);
+      return right(null);
+    } on Exception catch (e) {
+      return left(Failure(errorMessage: e.toString()));
+    }
   }
 }
